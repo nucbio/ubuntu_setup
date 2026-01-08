94ce1a913cc0d6c282e96d05c971b34a7c8ea9c8
Mark Boltengagen <m.boltengagen@gmail.com>
2026-01-08 07:03:21 +0100

    update

--- apps/elements.sh
+++ apps/elements.sh
@@ -5,7 +5,7 @@ if [[ "$INSTALL_OPT" != "Bio-WS" ]]; then
   sudo apt install -y wget apt-transport-https
   sudo wget -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages.element.io/debian/element-io-archive-keyring.gpg
   echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
-  sudo apt update
-  sudo apt install element-desktop
+  sudo apt update -y
+  sudo apt install -y element-desktop
 fi
 
