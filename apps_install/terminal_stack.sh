32e54154a72a2de3bbd4d694196eb74e5029b886
Mark Boltengagen <m.boltengagen@gmail.com>
2026-01-11 18:32:57 +0100

    tag_fix

--- apps_install/terminal_stack.sh
+++ apps_install/terminal_stack.sh
@@ -31,7 +31,7 @@ cargo install \
   --locked
 
 ## ripgrep - modern grep
-RIPGREP_RELEASE_TAG="${ALACRITTY_RELEASE_TAG:-15.1.0}"
+RIPGREP_RELEASE_TAG="${RIPGREP_RELEASE_TAG:-15.1.0}"
 # Install ripgrep
 cargo install \
   --git https://github.com/BurntSushi/ripgrep.git ripgrep\
